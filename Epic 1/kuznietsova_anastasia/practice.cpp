b5b2a9f5c9a730f70a3b27e78a696c9c36248cc7
anastasiakuznietsova <anastasiia.kuznietsova.shi.2023@lpnu.ua>
2023-10-26 18:31:57 +0300

    add descriptions

--- Epic 1/kuznietsova_anastasia/practice.cpp
+++ Epic 1/kuznietsova_anastasia/practice.cpp
@@ -16,14 +16,14 @@ int main ()
     cin>>d;
     cout<<"Enter the rate of interest (in percentage):\n";
     cin>>e;
-    e = e/100;
+    e = e/100; // перепис числа відсотків десятковим числом, щоб формула виконувалась правильно
     cout<<"Enter the number of years:\n";
     cin>>c;
     cout<<"Enter the number of times interest is compounded per year (e.g., 1 for annually, 4 for quarterly, 12 for monthly):\n";
     cin>>b;
 
-    g = d*pow(1+(e/b), c*b);
-    f = g-d;
+    g = d*pow(1+(e/b), c*b); // формула обчислення складних відсотків
+    f = g-d; // обрахунок повної суми до сплати після n років
 
     cout<<"Hello "<<a<<"!\n";
     cout<<"The compound interest for a principal of "<<d<<" at a rate of "<<e*100<<" compounded "<<b<<" times a year for "<<c<<" years is: "<<f<<"\n";
